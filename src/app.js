b6986067406d77c22af60b5f249b4d3574cd4ce9
gonzalohlh <gonzalo.galdame@gmail.com>
2024-08-20 19:51:05 -0300

    update

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ const dotenv = require("dotenv");
 const dbConnect = require("./config/dbConnect");
 const userRoute = require("./routes/users/usersRoute");
 const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
+const User = require("./model/User");
 
 const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    const usersApi = User.find();
+    res.json({
+      status: "success",
+      data: usersApi,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 dotenv.config();
 
