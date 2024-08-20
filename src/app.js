090da59063acd8f6bab13bf95176031b3137d35c
gonzalohlh <gonzalo.galdame@gmail.com>
2024-08-20 20:03:59 -0300

    update get

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.get("/", async (req, res) => {
   try {
-    const usersApi = User.find();
+    const usersApi = await User.find();
     res.json({
       status: "success",
       data: usersApi,
